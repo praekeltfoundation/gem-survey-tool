530df10e4142ed9ed4ce58bee0ee7c8c29b77c74
retrorabbitWD <wdawson@retrorabbit.co.za>
2015-02-11 16:02:01 +0200

    restructure

--- gems/settings.py
+++ gems/settings.py
@@ -127,6 +127,7 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django_hstore',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'gems.core',
