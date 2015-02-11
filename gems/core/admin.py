5b8e82af77873760457981c6f1817b69c3f44c2d
retrorabbitWD <wdawson@retrorabbit.co.za>
2015-02-11 15:48:04 +0200

    restructor files and add template

---
+++ gems/core/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from models import Survey, Contact, SurveyResults
+
+
+class SurveyAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('msisdn',)
+
+
+class SurveyResultsAdmin(admin.ModelAdmin):
+    list_display = ('survey_id','msisdn','answer')
+
+
+
+admin.site.register(Survey, SurveyAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(SurveyResults, SurveyResultsAdmin)
