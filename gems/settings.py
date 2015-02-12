8793485b5147fec982aa20b9315b35276f357b31
dbreedt <dbreedt@retrorabbit.co.za>
2015-02-12 11:16:57 +0200

    Changes to location of static paths

--- gems/settings.py
+++ gems/settings.py
@@ -68,11 +68,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = 'gems/core/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = 'core/static/'
+STATIC_URL = 'gems/core/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
