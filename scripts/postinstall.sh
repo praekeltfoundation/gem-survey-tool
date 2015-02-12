6be0c723ec5b88c63ade7d018c330cc4a535ef39
dbreedt <dbreedt@retrorabbit.co.za>
2015-02-12 12:14:58 +0200

    Added db create and hstore install sections to the script.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,4 +1,7 @@
 manage="${VENV}/bin/python ${INSTALLDIR}/${REPO}/manage.py"
 
+su - postgres -c "createdb gems"
+su - postgres -c "psql gems -c 'CREATE EXTENSION hstore;'"
+
 $manage syncdb --noinput --no-initial-data --migrate
 $manage collectstatic --noinput
