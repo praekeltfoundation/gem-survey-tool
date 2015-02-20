629b8f2f81e418d54943949a7731089383e63251
dbreedt <dbreedt@retrorabbit.co.za>
2015-02-20 14:04:03 +0200

    fixed minor issues in the other test cases. added a new testcase to test the csv export.

--- gems/core/tests.py
+++ gems/core/tests.py
@@ -8,7 +8,7 @@ from gems.core.models import *
 class RESTTestCase(TestCase):
     def test_save_data(self):
         c = Client()
-        J = {
+        j = {
             'user': {
                 'answers': {
                     'choice-1': 'value-4'
@@ -28,7 +28,51 @@ class RESTTestCase(TestCase):
             },
             'conversation_key': '1203491039401932804'
         }
-        result = c.post('/save_data/', content_type='application/json', data=json.dumps(J))
+        result = c.post('/save_data/', content_type='application/json', data=json.dumps(j))
         self.assertEquals(Survey.objects.count(), 1)
         self.assertEquals(SurveyResult.objects.count(), 1)
         self.assertEquals(Contact.objects.count(), 1)
+        
+    def test_csv_export(self):
+        c = Client()
+        j = {
+            'filters': [
+                {
+                    'field': {
+                        'name': 'age',
+                        'type': 'H'
+                    },
+                    'filters': [
+                        {
+                            'operator': 'lt',
+                            'value': '18'
+                        },
+                        {
+                            'loperator': 'or',
+                            'operator': 'gt',
+                            'value': '12'
+                        }
+                    ]
+                },
+                {
+                    'loperator': 'or',
+                    'field': {
+                        'name': 'Gender',
+                        'type': 'H'
+                    },
+                    'filters': [
+                        {
+                            'operator': 'eq',
+                            'value': 'female'
+                        }
+                    ]
+                }
+            ]
+        }
+
+        result = c.post('/export/', content_type='application/json', data=json.dumps(j))
+        self.assertEquals(result._headers['content-type'][1], 'text/csv')
+        self.assertEquals(result._headers['content-disposition'][1], 'attachment; filename=surveyresult_export.csv;')
+
+        result = c.get('/export/')
+        self.assertEquals(result._container[0], 'Bad request method')
