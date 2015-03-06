337a17a624ba5d75b0c033d9d8d8e21c16c430c0
msinovcic <msinovcic@retrorabbit.co.za>
2015-03-06 12:04:46 +0200

    Issue GST-16
    Added paging functionality.

--- gems/core/static/js/script.js
+++ gems/core/static/js/script.js
@@ -27,11 +27,18 @@ gems.config(function($interpolateProvider, $httpProvider){
     $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 });
 
-gems.controller('contentController', function ($scope, $http) {
+gems.controller('contentController', function ($scope, $http, $filter) {
     $scope.ContactGroups = {};
 
     $scope.group_id;
-    $scope.orderByField = 'name';
+
+    $scope.sortingOrder = 'name';
+    $scope.reverseSort = false;
+    $scope.filteredGroups = [];
+    $scope.pagedGroups = [];
+    $scope.currentPage = 1;
+    $scope.itemsPerPage = 20;
+    $scope.maxSize = 6;
 
     $scope.setGroupId =  function setGroupId(id){
         $scope.group_id = id;
@@ -41,6 +48,7 @@ gems.controller('contentController', function ($scope, $http) {
         $http.get('/contactgroup/').
             success(function(data) {
                 $scope.ContactGroups = data;
+                $scope.search();
             }).
             error(function(data) {
                 alert("failed to retreive data.");
@@ -67,6 +75,69 @@ gems.controller('contentController', function ($scope, $http) {
             error(function(status){
                 alert("failed"  + status);
             });
+    };
+
+        var searchMatch = function (haystack, needle){
+        if (!needle){
+            return true;
+        }
+        return haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1;
+    };
+
+    $scope.search = function() {
+        $scope.filteredGroups = $filter('filter')($scope.ContactGroups, function(item){
+            for(var attr in item){
+                if(searchMatch(item[attr], $scope.query))
+                    return true;
+            }
+            return false;
+        });
+
+        if ($scope.sortingOrder !== '') {
+            $scope.filteredGroups = $filter('orderBy')($scope.filteredGroups, $scope.sortingOrder, $scope.reverseSort);
+        }
+        $scope.currentPage = 0;
+        $scope.groupToPages();
+    };
+
+    $scope.groupToPages = function(){
+        $scope.pagedGroups = [];
+
+        for (var i = 0; i < $scope.filteredGroups.length; i++){
+            if (i % $scope.itemsPerPage === 0){
+                $scope.pagedGroups[Math.floor(i / $scope.itemsPerPage)] = [ $scope.filteredGroups[i] ];
+            }else{
+                $scope.pagedGroups[Math.floor(i / $scope.itemsPerPage)].push($scope.filteredGroups[i]);
+            }
+        }
+    };
+
+    $scope.range = function (start, end){
+        var ret = [];
+        if (!end){
+            end = start;
+            start = 0;
+        }
+        for (var i = start; i < end; i++){
+            ret.push(i);
+        }
+        return ret;
+    };
+
+    $scope.prevPage = function (){
+        if ($scope.currentPage > 0){
+            $scope.currentPage--;
+        }
+    };
+
+    $scope.nextPage = function (){
+        if ($scope.currentPage < $scope.pagedItems.length - 1){
+            $scope.currentPage++;
+        }
+    };
+
+    $scope.setPage = function (){
+        $scope.currentPage = this.n;
     };
 
     $scope.getContactGroups();
