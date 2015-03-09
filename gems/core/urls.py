6c6749b23663acd691f3bfe16305874277c2026d
dbreedt <dbreedt@retrorabbit.co.za>
2015-03-09 12:32:35 +0200

    Added the Sign out menu and functionality

--- gems/core/urls.py
+++ gems/core/urls.py
@@ -31,6 +31,8 @@ urlpatterns = patterns('',
 
     url(r'^login/$', user_login, name='login'),
 
+    url(r'^logout/$', user_logout, name='logout'),
+
     url(r'^', include(router.urls)),
 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
