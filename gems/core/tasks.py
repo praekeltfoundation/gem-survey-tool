84be0b857e0114cd5608ffc6e10aeebb67717448
msinovcic <msinovcic@retrorabbit.co.za>
2016-01-12 11:51:11 +0200

    Added a celery task to remove group members from the group

--- gems/core/tasks.py
+++ gems/core/tasks.py
@@ -9,7 +9,7 @@ import requests
 from go_http.contacts import ContactsApiClient
 from gems.core.models import Contact, ContactGroupMember
 from gems.core.viewhelpers import get_surveyresult_hstore_keys
-from gems.core.viewhelpers import process_group_member
+from gems.core.viewhelpers import process_group_member, remove_group_member
 
 logger = logging.getLogger(__name__)
 
@@ -211,6 +211,14 @@ def add_members_to_group(api, group, members):
             continue
         process_group_member(api, contact, group)
     logger.info('Adding members to %s group :: COMPLETED' % group.name)
+
+
+@task
+def remove_members_from_group(api, group, members):
+    logger.info('Removing members from %s group :: STARTED' % group.name)
+    for member in members:
+        remove_group_member(api, member, group)
+    logger.info('Removing members from %s group :: COMPLETED' % group.name)
 
 
 @task(bind=True)
